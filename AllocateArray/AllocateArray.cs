4dd706fc4a21622e8b597eacf5174d5b7f2bb4c2
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-11-04 11:13:36 +0200

    initial commit

---
+++ AllocateArray/AllocateArray.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace AllocateArray
+{
+    class AllocateArray
+    {
+        static void Main()
+        {
+            int number = int.Parse(Console.ReadLine());
+            int[] array = new int[number];
+            for (int i = 0; i < number; i++)
+            {
+                array[i] = i * 5;
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
---
+++ CompareArrays/CompareArrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+
+namespace CompareArrays
+{
+    class CompareArrays
+    {
+        static void Main(string[] args)
+        {
+            short n = short.Parse(Console.ReadLine());
+            int[] arrayOne = new int[n];
+            int[] arrayTwo = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                arrayOne[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                arrayTwo[i] = int.Parse(Console.ReadLine());
+            }
+            bool compare = true;
+            for (int i = 0; i < n; i++)
+            {
+                if (arrayOne[i] != arrayTwo[i])
+                {
+                    compare = false;
+                }
+            }
+            if (compare == false)
+            {
+                Console.WriteLine("Not equal");
+            }
+            else
+            {
+                Console.WriteLine("Equal");
+            }
+        }
+    }
+}
