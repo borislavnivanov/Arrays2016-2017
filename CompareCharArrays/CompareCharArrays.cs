7b3661064e826fe269fb8316a8e892dbe8362bcc
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-11-05 22:32:46 +0200

    j

---
+++ CompareCharArrays/CompareCharArrays.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+
+namespace CompareCharArrays
+{
+    class CompareCharArrays
+    {
+        static void Main()
+        {
+            char[] inputOne = Console.ReadLine().ToCharArray();
+            char[] inputTwo = Console.ReadLine().ToCharArray();
+            int lengthComp = inputOne.Length.CompareTo(inputTwo.Length);
+
+            if (lengthComp > 0)
+            {
+                Console.WriteLine(">");
+            }
+            else if (lengthComp == 0)
+            {
+                Console.WriteLine("=");
+            }
+            else
+            {
+                Console.WriteLine("<");
+            }
+        }
+    }
+}
