8eed1e0d97ac35034958a8e789bd20a38bce063a
Borislav Nikolaev Ivanov <borislavnivanov@gmail.com>
2016-11-09 00:56:19 +0200

    fsg

---
+++ MaximalSum/MaximalSum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSum
+{
+    class MaximalSum
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ SelectionSort/SelectionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace SelectionSort
+{
+    class SelectionSort
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            numbers.Sort();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
